7d151198e24bc1b62b776eb8aee5dbbd3e811a70
SUNEEL KUMAR <98082588+suneelkumarprathipati@users.noreply.github.com>
2023-12-27 15:11:08 +0530

    Time: 5 ms (99.49%), Space: 61.1 MB (11.83%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
@@ -1,26 +1,18 @@
-import java.util.Arrays;
 
-public class Solution {
+class Solution {
     public int minCost(String colors, int[] neededTime) {
-        int n = colors.length();
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
-        return calculateMinCost(n - 1, colors, neededTime, dp, 'A', neededTime[n - 1]);
-    }
-
-    private int calculateMinCost(int i, String colorSequence, int[] timeRequired, int[] memo, char previousColor, int previousTime) {
-        if (i < 0) {
-            return 0;
+        int needed=0;
+        int n=neededTime.length;
+        char[] chc=colors.toCharArray();
+        int cur_max=neededTime[0];
+        for(int i=1;i<n;i++){
+            if(chc[i-1]!=chc[i]){
+                cur_max=neededTime[i];
+            }else{
+                needed+=Math.min(cur_max, neededTime[i]);
+                cur_max=Math.max(cur_max, neededTime[i]);
+            }
         }
-
-        if (memo[i] != -1) {
-            return memo[i];
-        }
-
-        if (colorSequence.charAt(i) == previousColor) {
-            return memo[i] = calculateMinCost(i - 1, colorSequence, timeRequired, memo, colorSequence.charAt(i), Math.max(timeRequired[i], previousTime)) + Math.min(timeRequired[i], previousTime);
-        } else {
-            return memo[i] = calculateMinCost(i - 1, colorSequence, timeRequired, memo, colorSequence.charAt(i), timeRequired[i]);
-        }
+        return needed;
     }
 }
